244f69df07e7c4d84e8ffe7cfca79a10506dce3d
camilapadilha <camilapadilha1997@gmail.com>
2019-07-19 18:53:54 -0300

    first commit

---
+++ db1go-pvai-client/src/views/Cadastro.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+
+  <v-form ref="form">
+        <v-flex xs12 md4 ml-5>
+            <v-text-field
+            label="Name"
+            ></v-text-field>
+        </v-flex>
+        <v-flex xs12 md4 ml-5>
+            <v-text-field
+            label="Date"
+            ></v-text-field>
+        </v-flex>
+        <v-flex xs12 md4 ml-5>
+            <v-text-field
+            label="Start Value"
+            ></v-text-field>
+        </v-flex>
+        <v-flex xs12 md4 ml-5>
+            <v-text-field
+            label="End Value"
+            ></v-text-field>
+        </v-flex>
+        
+        <v-btn color="info">Salvar</v-btn>
+        
+  </v-form>
+  </div>
+</template>
+<script lang="ts">
+import {Vue, Component} from 'vue-property-decorator'
+
+@Component({})
+export default class Cadastro extends Vue {
+    
+}
+</script>
+<style lang="sass" scoped>
+
+</style>
+
+
+
---
+++ db1go-pvai-client/src/views/Consulta.vue
@@ -0,0 +1,66 @@
+<template>
+    <v-data-table
+    :headers="headers"
+    :items="desserts"
+    class="elevation-1"
+  >
+    <template v-slot:items="props">
+      <td>{{ props.item.name }}</td>
+      <td>{{ props.item.date }}</td>
+      <td>{{ props.item.start }}</td>
+      <td>{{ props.item.end}}</td>
+    </template>
+  </v-data-table>
+</template>
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component({})
+export default class Consulta extends Vue {}
+</script>
+<style lang="sass" scoped>
+
+</style>
+
+<script>
+  export default {
+    data () {
+      return {
+        headers: [
+          {
+            text: 'Name',
+            sortable: false,
+            value: 'name'
+          },
+          { text: 'Date', value: 'data'},
+          { text: 'Start Value', value: 'start'},
+          { text: 'End Value', value: 'end'}
+
+        ],
+        desserts: [
+          
+          {
+            name: 'Frozen Yogurt',
+            date: "15-02-2019",
+            start: 6.0,
+            end: 24
+
+          },
+          {
+            name: 'Ice cream sandwich',
+            date: "20-02-2019",
+            start: 9.0,
+            end: 37
+          },
+          {
+            name: 'Eclair',
+            date: "26-01-2019",
+            start: 16.0,
+            end: 23
+          }
+        ]
+      }
+    }
+  }
+</script>
+
